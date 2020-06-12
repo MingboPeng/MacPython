f993ad89dbb30b4462899bd7bc4fa7beead6990e
Mingbo Peng <mingo1214@gmail.com>
2020-06-12 19:59:07 -0400

    fix(path): build to user's path instead of system's

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ set -e
 curl -O  https://www.openssl.org/source/openssl-1.1.1g.tar.gz
 tar -zxvf openssl-1.1.1g.tar.gz
 cd openssl-1.1.1g
-./config --prefix=/usr/local/pythonssl    # prefix : where to install
+./config --prefix=/Users/runner/Desktop/pythonssl    # prefix : where to install
 sudo make -j8
 sudo make install -j8
 
@@ -19,35 +19,36 @@ cd Python-3.8.3
 # copy Setup to Modules
 cp ../Setup ./Modules
 
-./configure --prefix=/usr/local/python --enable-optimizations
+./configure --prefix=/Users/runner/Desktop/python --enable-optimizations
 sudo make -j8
 sudo make install -j8
 
-sudo cp /usr/local/pythonssl/lib/lib* /usr/local/python/lib/
+sudo cp /Users/runner/Desktop/pythonssl/lib/lib* /Users/runner/Desktop/python/lib/
 # copy libssl.1.1.dylib and libcrypto.1.1.dylib to python/lib
 
-cd /usr/local/python/
+cd /Users/runner/Desktop/python/
 # check links in python
 otool -l bin/python3
 #Fix python's link
-sudo install_name_tool -change /usr/local/pythonssl/lib/libssl.1.1.dylib  @loader_path/../lib/libssl.1.1.dylib bin/python3 
-sudo install_name_tool -change /usr/local/pythonssl/lib/libcrypto.1.1.dylib  @loader_path/../lib/libcrypto.1.1.dylib bin/python3 
+sudo install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libssl.1.1.dylib  @loader_path/../lib/libssl.1.1.dylib bin/python3 
+sudo install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libcrypto.1.1.dylib  @loader_path/../lib/libcrypto.1.1.dylib bin/python3 
 otool -l bin/python3
 
 #fix ssl lib's lik
 otool -l lib/libssl.1.1.dylib
-sudo install_name_tool -change /usr/local/pythonssl/lib/libssl.1.1.dylib  @loader_path/../lib/libssl.1.1.dylib lib/libssl.1.1.dylib
-sudo install_name_tool -change /usr/local/pythonssl/lib/libcrypto.1.1.dylib  @loader_path/../lib/libcrypto.1.1.dylib lib/libssl.1.1.dylib
+sudo install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libssl.1.1.dylib  @loader_path/../lib/libssl.1.1.dylib lib/libssl.1.1.dylib
+sudo install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libcrypto.1.1.dylib  @loader_path/../lib/libcrypto.1.1.dylib lib/libssl.1.1.dylib
 
 
 # otool -l lib/libcrypto.1.1.dylib
-# install_name_tool -change /usr/local/pythonssl/lib/libcrypto.1.1.dylib  @loader_path/../lib/libcrypto.1.1.dylib lib/libcrypto.1.1.dylib
+# install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libcrypto.1.1.dylib  @loader_path/../lib/libcrypto.1.1.dylib lib/libcrypto.1.1.dylib
 ls
 
 sudo bin/python3 -m pip install --upgrade pip
 
 # pack python
 cd ../
+# /Users/runner/Desktop/
 sudo pkgbuild --identifier macpython.pkg --root "python" --install-location "Applications/MacPython" MacPython.pkg
 sudo zip -r python_osx.zip MacPython.pkg
 
