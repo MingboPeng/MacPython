086aad32b08c6e838ec74eb6f72c5001719a8337
MingboPeng <mingo1214@gmail.com>
2023-10-20 15:36:27 +0800

    finish python 3.10.10

--- build.sh
+++ build.sh
@@ -1,60 +1,33 @@
 set -e
 
-
-# download ssl source, compile
-curl -O  https://www.openssl.org/source/openssl-1.1.1g.tar.gz
-tar -zxvf openssl-1.1.1g.tar.gz
-cd openssl-1.1.1g
-./config --prefix=/Users/runner/Desktop/pythonssl    # prefix : where to install
-sudo make -j8
-sudo make install -j8
+echo "pythonVersion: $1";
+echo "pythonMajorVersion: $2";
 
 
 # download python source, compile
-cd ../
-curl -O  https://www.python.org/ftp/python/3.8.3/Python-3.8.3.tgz
-tar -zxvf Python-3.8.3.tgz
-cd Python-3.8.3
-
-# copy Setup to Modules
-cp ../Setup ./Modules
+echo "Download Python";
+curl -O  https://www.python.org/ftp/python/$1/Python-$1.tgz
+tar -zxvf Python-$1.tgz
+cd Python-$1
 
 ./configure --prefix=/Users/runner/Desktop/python --enable-optimizations
 sudo make -j8
 sudo make install -j8
 
-# copy libssl.1.1.dylib and libcrypto.1.1.dylib to python/lib
-sudo cp /Users/runner/Desktop/pythonssl/lib/lib* /Users/runner/Desktop/python/lib/
-
-
 
 cd /Users/runner/Desktop/python/
 
 
-# check links in python
-otool -l bin/python3
-#Fix python's link
-sudo install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libssl.1.1.dylib  @loader_path/../lib/libssl.1.1.dylib bin/python3 
-sudo install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libcrypto.1.1.dylib  @loader_path/../lib/libcrypto.1.1.dylib bin/python3 
-otool -l bin/python3
-
-#fix ssl lib's lik
-otool -l lib/libssl.1.1.dylib
-sudo install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libssl.1.1.dylib  @loader_path/../lib/libssl.1.1.dylib lib/libssl.1.1.dylib
-sudo install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libcrypto.1.1.dylib  @loader_path/../lib/libcrypto.1.1.dylib lib/libssl.1.1.dylib
-
-
-# otool -l lib/libcrypto.1.1.dylib
-# install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libcrypto.1.1.dylib  @loader_path/../lib/libcrypto.1.1.dylib lib/libcrypto.1.1.dylib
 ls
-
+echo "install pip";
 sudo bin/python3 -m pip install --upgrade pip
 
-#make bin/python alias pointing to python3.8
+#make bin/python alias pointing to python3.7
 cd bin
-ln -s python3.8 python
+ln -s python$2 python
 
 # pack python
+echo "pack python";
 cd ../../
 # /Users/runner/Desktop/
 sudo pkgbuild --identifier macpython.pkg --root "python" --install-location "Applications/MacPython" MacPython.pkg
--- build379.sh
+++ build379.sh
@@ -1,66 +1,60 @@
 set -e
 
-echo "pythonVersion: $1";
-echo "pythonMajorVersion: $2";
 
-# # download ssl source, compile
-# echo "Download SSL";
-# curl -O  https://www.openssl.org/source/openssl-1.1.1g.tar.gz
-# tar -zxvf openssl-1.1.1g.tar.gz
-# cd openssl-1.1.1g
-# ./config --prefix=/Users/runner/Desktop/pythonssl    # prefix : where to install
-# sudo make -j8
-# sudo make install -j8
-# cd ../
+# download ssl source, compile
+curl -O  https://www.openssl.org/source/openssl-1.1.1g.tar.gz
+tar -zxvf openssl-1.1.1g.tar.gz
+cd openssl-1.1.1g
+./config --prefix=/Users/runner/Desktop/pythonssl    # prefix : where to install
+sudo make -j8
+sudo make install -j8
 
 
 # download python source, compile
-echo "Download Python";
-curl -O  https://www.python.org/ftp/python/$1/Python-$1.tgz
-tar -zxvf Python-$1.tgz
-cd Python-$1
+cd ../
+curl -O  https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz
+tar -zxvf Python-3.7.9.tgz
+cd Python-3.7.9
 
-# # copy Setup to Modules
-# echo "copy Setup to Modules";
-# cp ../Setup.dist ./Modules
+# copy Setup to Modules
+cp ../Setup.dist ./Modules
 
 ./configure --prefix=/Users/runner/Desktop/python --enable-optimizations
 sudo make -j8
 sudo make install -j8
 
-# # copy libssl.1.1.dylib and libcrypto.1.1.dylib to python/lib
-# echo "copy libssl.1.1.dylib and libcrypto.1.1.dylib to python/lib";
-# sudo cp /Users/runner/Desktop/pythonssl/lib/lib* /Users/runner/Desktop/python/lib/
+# copy libssl.1.1.dylib and libcrypto.1.1.dylib to python/lib
+sudo cp /Users/runner/Desktop/pythonssl/lib/lib* /Users/runner/Desktop/python/lib/
 
 
 
 cd /Users/runner/Desktop/python/
 
 
-# # check links in python
-# echo "check links in python";
-# otool -l bin/python3
-# #Fix python's link
-# sudo install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libssl.1.1.dylib  @loader_path/../lib/libssl.1.1.dylib bin/python3 
-# sudo install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libcrypto.1.1.dylib  @loader_path/../lib/libcrypto.1.1.dylib bin/python3 
-# otool -l bin/python3
+# check links in python
+otool -l bin/python3
+#Fix python's link
+sudo install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libssl.1.1.dylib  @loader_path/../lib/libssl.1.1.dylib bin/python3 
+sudo install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libcrypto.1.1.dylib  @loader_path/../lib/libcrypto.1.1.dylib bin/python3 
+otool -l bin/python3
 
-# #fix ssl lib's lik
-# otool -l lib/libssl.1.1.dylib
-# sudo install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libssl.1.1.dylib  @loader_path/../lib/libssl.1.1.dylib lib/libssl.1.1.dylib
-# sudo install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libcrypto.1.1.dylib  @loader_path/../lib/libcrypto.1.1.dylib lib/libssl.1.1.dylib
+#fix ssl lib's lik
+otool -l lib/libssl.1.1.dylib
+sudo install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libssl.1.1.dylib  @loader_path/../lib/libssl.1.1.dylib lib/libssl.1.1.dylib
+sudo install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libcrypto.1.1.dylib  @loader_path/../lib/libcrypto.1.1.dylib lib/libssl.1.1.dylib
 
 
+# otool -l lib/libcrypto.1.1.dylib
+# install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libcrypto.1.1.dylib  @loader_path/../lib/libcrypto.1.1.dylib lib/libcrypto.1.1.dylib
 ls
-echo "install pip";
+
 sudo bin/python3 -m pip install --upgrade pip
 
 #make bin/python alias pointing to python3.7
 cd bin
-ln -s python$2 python
+ln -s python3.7 python
 
 # pack python
-echo "pack python";
 cd ../../
 # /Users/runner/Desktop/
 sudo pkgbuild --identifier macpython.pkg --root "python" --install-location "Applications/MacPython" MacPython.pkg
