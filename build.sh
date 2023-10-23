2cb0aaf7d68fb68d5ebf2b2cf4febd3ecbf4060c
MingboPeng <mingo1214@gmail.com>
2023-10-23 19:58:43 +0800

    fix ssl

--- build.sh
+++ build.sh
@@ -3,15 +3,6 @@ set -e
 echo "pythonVersion: $1";
 echo "pythonMajorVersion: $2";
 
-# download ssl source, compile
-echo "Download openssl";
-curl -O  https://www.openssl.org/source/openssl-1.1.1g.tar.gz
-tar -zxvf openssl-1.1.1g.tar.gz
-cd openssl-1.1.1g
-./config --prefix=/Users/runner/Desktop/pythonssl    # prefix : where to install
-sudo make -j8
-sudo make install -j8
-
 
 cd ../
 
@@ -32,8 +23,9 @@ sudo make install -j8
 # copy libintl.8.dylib to python/lib
 sudo cp /usr/local/opt/gettext/lib/libintl.8.dylib /Users/runner/Desktop/python/lib/
 
-# copy libssl.1.1.dylib and libcrypto.1.1.dylib to python/lib
-sudo cp /Users/runner/Desktop/pythonssl/lib/lib* /Users/runner/Desktop/python/lib/
+# copy libssl.3.dylib and libcrypto.3.dylib to python/lib
+sudo cp /usr/local/opt/openssl@3/lib/libssl.3.dylib /Users/runner/Desktop/python/lib/
+sudo cp /usr/local/opt/openssl@3/lib/libcrypto.3.dylib /Users/runner/Desktop/python/lib/
 
 
 cd /Users/runner/Desktop/python/
@@ -43,15 +35,10 @@ otool -l bin/python3
 #Fix python's link: libintl.8
 sudo install_name_tool -change /usr/local/opt/gettext/lib/libintl.8.dylib  @loader_path/../lib/libintl.8.dylib bin/python3
 
-#Fix python's link: libssl.1.1.dylib and libcrypto.1.1.dylib
-sudo install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libssl.1.1.dylib  @loader_path/../lib/libssl.1.1.dylib bin/python3 
-sudo install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libcrypto.1.1.dylib  @loader_path/../lib/libcrypto.1.1.dylib bin/python3 
+#Fix python's link: libssl.3.dylib and libcrypto.3.dylib
+sudo install_name_tool -change /usr/local/opt/openssl@3/lib/libssl.3.dylib  @loader_path/../lib/libssl.3.dylib lib/python3.10/lib-dynload/_ssl.cpython-310-darwin.so
+sudo install_name_tool -change /usr/local/opt/openssl@3/lib/libcrypto.3.dylib  @loader_path/../lib/libcrypto.3.dylib lib/python3.10/lib-dynload/_ssl.cpython-310-darwin.so
 otool -l bin/python3
-
-#fix ssl lib's lik
-otool -l lib/libssl.1.1.dylib
-sudo install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libssl.1.1.dylib  @loader_path/../lib/libssl.1.1.dylib lib/libssl.1.1.dylib
-sudo install_name_tool -change /Users/runner/Desktop/pythonssl/lib/libcrypto.1.1.dylib  @loader_path/../lib/libcrypto.1.1.dylib lib/libssl.1.1.dylib
 
 
 ls
@@ -68,5 +55,3 @@ cd ../../
 # /Users/runner/Desktop/
 sudo pkgbuild --identifier macpython.pkg --root "python" --install-location "Applications/MacPython" MacPython.pkg
 sudo zip -r MacPython.zip MacPython.pkg
-
-
